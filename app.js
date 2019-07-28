c6730f51e6039e2ffe49fa5532cfc0de30d7a5cb
Victoria <45143789+Inseteco@users.noreply.github.com>
2019-07-28 20:26:57 +0100

    Sort
    Table Sort

---
+++ app.js
@@ -0,0 +1,28 @@
+new Vue ({
+  el:'#app',
+
+  data () {
+    return {
+    numdata:[
+      [3, 4, 7],
+      [6, 1, 2],
+      [1, 9, 1]
+    ],
+    numb:
+    [false, false,false]
+  }},
+
+  methods: {
+    sortBy(e){
+      if(this.numb) {
+        this.numb = false;
+        this.numdata.sort((a,b) =>
+        a[e] - b[e])
+      } else{
+        this.numb = true;
+        this.numdata.sort((a,b) =>
+        b[e] - a[e])
+      }
+    }
+  }
+})

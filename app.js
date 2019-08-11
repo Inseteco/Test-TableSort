baf6c394b787f69857207cd38286ab1e82cd4310
Victoria <45143789+Inseteco@users.noreply.github.com>
2019-08-11 18:13:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,25 +3,27 @@ new Vue ({
 
   data () {
     return {
-    numdata:[
-      [3, 4, 7],
-      [6, 1, 2],
-      [1, 9, 1]
-    ],
-    numb:
-    [false, false,false]
-  }},
+      numdata:[
+        [3, 4, 7],
+        [6, 1, 2],
+        [1, 9, 1]
+      ],
+      numb:[
+        false,
+        false,
+        false
+      ]
+    }
+  },
 
   methods: {
     sortBy(e){
       if(this.numb) {
         this.numb = false;
-        this.numdata.sort((a,b) =>
-        a[e] - b[e])
+        this.numdata.sort((a,b) => a[e] - b[e])
       } else{
         this.numb = true;
-        this.numdata.sort((a,b) =>
-        b[e] - a[e])
+        this.numdata.sort((a,b) => b[e] - a[e])
       }
     }
   }
